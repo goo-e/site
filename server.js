4db4acad80c9472238bcafe976d351a81df53dcd
graygabrielle <gray.atkinson.adams@gmail.com>
2019-04-29 15:56:08 -0700

    connected to MongoDB atlas

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
-const mongoose = require("mongoose");
+//connect database
+connectDB();
+
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
-const dbName = "appDB";
 
-app.use(express.urlencoded( { extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODV_URI || `mongodb://localhost/${dbName}`, { useNewUrlParser: true } );
-
 app.listen(PORT, function() {
-    console.log(`API server now listening on port ${PORT}`);
+  console.log(`API server now listening on port ${PORT}`);
 });
+
+//======probably don't need this stuff anymore - see config/db.js=======
+
+// const mongoose = require("mongoose");
+
+// const dbName = "appDB";
+
+// mongoose.connect(process.env.MONGODV_URI || `mongodb://localhost/${dbName}`, {
+//   useNewUrlParser: true
+// });
