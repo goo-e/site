6f621c6f3afa55a880450cc2c8a9223f940288bf
graygabrielle <gray.atkinson.adams@gmail.com>
2019-04-29 23:46:10 -0700

    added ability to log in and get token (before was register and token)

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const authRouter = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const { check, validationResult } = require("express-validator/check");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
 
 //@route   GET api/auth
 //@desc    Test route
@@ -15,5 +19,62 @@ authRouter.get("/", auth, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+//@route   POST api/auth
+//@desc    Authenticate user and get token
+//@access  Public
+authRouter.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      //check if password is correct
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+      //return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 module.exports = authRouter;
