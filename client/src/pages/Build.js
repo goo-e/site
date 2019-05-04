6c72b4cb2e3edc669e7eac5fa1ae99589280bb89
Cameron <cameronjmaccabe@gmail.com>
2019-05-03 20:09:25 -0700

    added new param types and the functionality to render them as appropriate

--- client/src/pages/Build.js
+++ client/src/pages/Build.js
@@ -1,71 +1,31 @@
 import React, { Component } from "react";
-import { FormInput, SubmitQuery } from "../components";
+import { FormInput, SelectInput, SubmitQuery } from "../components";
 
-// An array containing every parameter on the menu.
-const paramsArr = [
-    {
-        name: "multiTerm",
-        type: "FormInput",
-        value: "",
-        querySegment: value => {return value ? `&as_q=${value.replace(/\s+/g, "+")}` : ""}
-    },
-    {
-        name: "exactMatch",
-        type: "FormInput",
-        value: "",
-        querySegment: value => {return value ? `&as_epq=${value.replace(/\s+/g, "+")}` : ""}
-    },
-    {
-        name: "anyOfThese",
-        type: "FormInput",
-        value: "",
-        querySegment: value => {return value ? `&as_oq=%28${value.replace(/\s+/g, "+")}%29` : ""}
-    },
-    {
-        name: "noneOfThese",
-        type: "FormInput",
-        value: "",
-        querySegment: value => {return value ? `&as_eq=${value.replace(/\s+/g, "+")}` : ""}
-    },
-    {
-        name: "numericalRange",
-        type: "Range",
-        value: ["", ""],
-        querySegment: value => {
-            const querySegment = value.map((val, index) => {
-                if (index === 0) {
-                    return val ? `&as_nlo=${val}` : "";
-                }
-                return val ? `&as_nhi=${val}` : "";
-            }).join("");
-            return querySegment;
-        }
-    }
-];
+const paramsArr = require("../paramsArr");
 
 // An array containing only those parameters that are set to be displayed by default in the user's prefs.
 // Until we get the user-specific information from the database, we will use the following as the default/placeholder:
 const prefsArr = [
     {
-        name: "multiTerm",
+        name: "Search Term(s)",
         type: "FormInput",
         value: "",
         querySegment: value => {return value ? `&as_q=${value.replace(/\s+/g, "+")}` : ""}
     },
     {
-        name: "exactMatch",
+        name: "Exact Match",
         type: "FormInput",
         value: "",
         querySegment: value => {return value ? `&as_epq=${value.replace(/\s+/g, "+")}` : ""}
     },
     {
-        name: "anyOfThese",
+        name: "Include Any",
         type: "FormInput",
         value: "",
         querySegment: value => {return value ? `&as_oq=%28${value.replace(/\s+/g, "+")}%29` : ""}
     },
     {
-        name: "noneOfThese",
+        name: "Exclude Each",
         type: "FormInput",
         value: "",
         querySegment: value => {return value ? `&as_eq=${value.replace(/\s+/g, "+")}` : ""}
@@ -94,10 +54,22 @@ class Build extends Component {
             case "Range":
                 value = ["", ""];
                 break;
+            case "RangeWithUnits":
+                value = ["", "", ""];
+                break;
+            case "Select":
+                value = "";
+                break;
             default:
                 return;
         }
-        const newParam = {
+        const newParam = param.type === "Select" ? {
+            name: param.name,
+            type: param.type,
+            options: param.options,
+            value: value,
+            querySegment: param.querySegment
+        } : {
             name: param.name,
             type: param.type,
             value: value,
@@ -130,8 +102,11 @@ class Build extends Component {
         // Here we use the unique editKey to target the param currently being edited by the user and save it for later reference.
         // On deletion of the corresponding param, we set the value of our edited object to undefined in the case of a FormInput param type
         const edited = this.state.params[this.state.editKey] || { value: undefined };
-        // or an array of two undefined elements in the case of a Range param type.
+        // or to an array of two undefined elements in the case of a Range param type
         const editedRange = this.state.params[this.state.editKey] || { value: [undefined, undefined] };
+        //or to an array of three undefined elements in the case of a RangeWithUnits param type
+        const editedRangeWithUnits = this.state.params[this.state.editKey] || { value: [undefined, undefined, undefined] };
+
         switch(type) {
             case "FormInput":
                 return (
@@ -159,6 +134,40 @@ class Build extends Component {
                         />
                     </div>
                 );
+            case "RangeWithUnits":
+                return (
+                    <div>
+                        <FormInput
+                            name={this.state.edit.name + "Low"}
+                            value={editedRangeWithUnits.value[0]}
+                            onChange={event => this.handleRangeChange(event, 0)}
+                            placeholder={editedRangeWithUnits.value[0]}
+                        />
+                        <FormInput
+                            name={this.state.edit.name + "High"}
+                            value={editedRangeWithUnits.value[1]}
+                            onChange={event => this.handleRangeChange(event, 1)}
+                            placeholder={editedRangeWithUnits.value[1]}
+                        />
+                        <FormInput
+                            name={this.state.edit.name + editedRangeWithUnits.value[2]}
+                            value={editedRangeWithUnits.value[2]}
+                            onChange={event => this.handleRangeChange(event, 2)}
+                            placeholder={editedRangeWithUnits.value[2]}
+                        />
+                    </div>
+                );
+            case "Select":
+                console.log(this.state.edit);
+                return (
+                    <SelectInput
+                        name={this.state.edit.name}
+                        options={this.state.edit.options}
+                        value={edited.value}
+                        onChange={this.handleOptionChange}
+                        placeholder={edited.value}
+                    />
+                );
             default:
                 return;
         }
@@ -178,6 +187,15 @@ class Build extends Component {
         const { value } = event.target;
         const updatedParams = this.state.params;
         updatedParams[this.state.editKey].value[index] = value;
+        this.setState({
+            params: updatedParams
+        });
+        console.log(this.state);
+    };
+
+    handleOptionChange = value => {
+        const updatedParams = this.state.params;
+        updatedParams[this.state.editKey].value = value;
         this.setState({
             params: updatedParams
         });
@@ -205,7 +223,7 @@ class Build extends Component {
                         return (
                             <div>
                                 <button key={index} name={param.name}>
-                                    <span onClick={() => this.edit(param, index)}>{param.name}</span>
+                                    <span onClick={() => this.state.edit !== param ? this.edit(param, index) : this.edit("", "")}>{param.name}</span>
                                     <span onClick={() => this.removeBtn(index)}>X</span>
                                 </button>
                                 {this.state.edit === param ? this.renderSwitch(param.type) : console.log("No param selected.")}
