fe27526ae577c3e04a514db52ed52c9a51f9dd76
graygabrielle <gray.atkinson.adams@gmail.com>
2019-05-05 12:00:02 -0700

    removed expiration from tokens

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -60,15 +60,10 @@ authRouter.post(
         }
       };
 
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 3600 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
+      jwt.sign(payload, config.get("jwtSecret"), (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (err) {
       console.error(err.message);
       res.status(500).send("server error");
--- routes/api/user.js
+++ routes/api/user.js
@@ -61,15 +61,10 @@ userRouter.post(
         }
       };
 
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 360000000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
+      jwt.sign(payload, config.get("jwtSecret"), (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (err) {
       console.error(err.message);
       res.status(500).send("server error");
