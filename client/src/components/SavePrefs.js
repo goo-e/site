d8853789fbdface8f46a4119cb3b0c7d573b8fdc
graygabrielle <gray.atkinson.adams@gmail.com>
2019-05-05 01:08:06 -0700

    set button to inherit props from parent

--- client/src/components/SavePrefs.js
+++ client/src/components/SavePrefs.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SavePrefs = () => {
-  return (
-    <div>
-        <button>Save Changes</button>
-    </div>
-  );
+class SavePrefs extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.onClick}>Save Changes</button>
+      </div>
+    );
+  }
 }
 
 export default SavePrefs;
