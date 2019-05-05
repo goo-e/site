3a4c4aab03371297fb83fa366322136a1205ad2c
graygabrielle <gray.atkinson.adams@gmail.com>
2019-05-05 11:55:29 -0700

    removed console log

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,7 +13,6 @@ authRouter.get("/", auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
     res.json(user);
-    console.log("hit this");
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
