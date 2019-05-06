433a07dfeed298c45c421be678ba2e5cd64cd624
graygabrielle <gray.atkinson.adams@gmail.com>
2019-05-05 20:21:51 -0700

    conditional navbars

--- client/src/GlobalApp.js
+++ client/src/GlobalApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import About from "./pages/About";
 import Build from "./pages/Build";
 import EditAccount from "./pages/EditAccount";
@@ -25,6 +25,12 @@ class GlobalApp extends Component {
                     <Logo />
                     <LoginBtn />
                     <RegisterBtn />
+                    <Link to="/build">
+                      <p>build</p>
+                    </Link>
+                    <Link to="/about">
+                      <p>about</p>
+                    </Link>
                   </nav>
                   <div className="App-header">
                     <Switch>
@@ -47,6 +53,15 @@ class GlobalApp extends Component {
                   <nav>
                     <Logo />
                     <LogoutBtn />
+                    <Link to="/prefs">
+                      <p>preferences</p>
+                    </Link>
+                    <Link to="/build">
+                      <p>build</p>
+                    </Link>
+                    <Link to="/about">
+                      <p>about</p>
+                    </Link>
                   </nav>
                   <div className="App-header">
                     <Switch>
