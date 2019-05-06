c6533753f8fcff30ce0c9548509b355182e295ee
graygabrielle <gray.atkinson.adams@gmail.com>
2019-05-05 17:37:56 -0700

    added logout functionality: removed cookie, cleared headers, updated global state

--- client/src/components/LogoutBtn.js
+++ client/src/components/LogoutBtn.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Consumer } from "../context";
+import Cookies from "universal-cookie";
+import setAuthToken from "../utils/setAuthToken";
 import API from "../utils/API";
+const cookies = new Cookies();
 
 class LogoutBtn extends Component {
+  onClick(dispatch) {
+    cookies.remove("token");
+    localStorage.removeItem("token");
+    setAuthToken();
+    dispatch({
+      type: "CLEAR_USER"
+    });
+  }
+
   render() {
     return (
-      <Link to="/login">
-        <button>Log out</button>
-      </Link>
+      <Consumer>
+        {value => {
+          const { dispatch, isAuthenticated } = value;
+          return (
+            <Link to="/login">
+              <button onClick={() => this.onClick(dispatch)}>Log out</button>
+            </Link>
+          );
+        }}
+      </Consumer>
     );
   }
 }
